1c42fbe3e6c958bf2b40ae8184ebf85c57bc3780
med363 <blibechmohamedamine97@gmail.com>
2025-12-22 16:44:17 +0100

    create exception in comuser if ressource leaks or anymore

--- consumer.py
+++ consumer.py
@@ -11,17 +11,24 @@ ConsumerConfig = Consumer({
 #subscribe to the 'orders' topic to consume messages
 ConsumerConfig.subscribe(['orders'])
 print("Consumer is listening to 'orders' topic...")
-#poll for new messages from the topic
-while True:
-    msg = ConsumerConfig.poll(1.0)  # timeout set to 1 second
-    if msg is None:
-        continue  # no message received, continue polling
-    if msg.error():
-        print(f"Consumer error: {msg.error()}")
-        continue
-    #process the received message
-    value = msg.value().decode('utf-8')
-    #convert the json string back to a Python object
-    orders = json.loads(value)
-    #display the received order
-    print(f"Received order: {orders['quantity']} x {orders['item_name']} for {orders['customer_name']} (Order ID: {orders['order_id']})")  
+try:
+        #poll for new messages from the topic
+        while True:
+            msg = ConsumerConfig.poll(1.0)  # timeout set to 1 second
+            if msg is None:
+                continue  # no message received, continue polling
+            if msg.error():
+                print(f"Consumer error: {msg.error()}")
+                continue
+            #process the received message
+            value = msg.value().decode('utf-8')
+            #convert the json string back to a Python object
+            orders = json.loads(value)
+            #display the received order
+            print(f"Received order: {orders['quantity']} x {orders['item_name']} for {orders['customer_name']} (Order ID: {orders['order_id']})")  
+
+except KeyboardInterrupt:
+    pass
+finally:
+    #close the consumer to release resources
+    ConsumerConfig.close()  
